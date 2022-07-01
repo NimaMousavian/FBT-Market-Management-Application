2521a481a389f6babb04331c2dd16bce74b8b596
Nima Mousavian <mosaviannima@gmail.com>
2022-07-01 18:50:35 +0430

    login page ui updated

--- source/login_page.cpp
+++ source/login_page.cpp
@@ -9,7 +9,31 @@ Login_page::Login_page(QMainWindow *mw,QWidget *parent) :
     ui->setupUi(this);
     this->mainwindow = mw;
 
+    //----------------- set push buttons icons ------------------
+
     ui->back_push->setIcon(QIcon("E:/C++_T2/FBT_Gui/icons/go back.png"));
+    ui->login_push->setIcon(QIcon("E:/C++_T2/FBT_Gui/icons/login2.png"));
+    ui->login_push->setIconSize(QSize(90,90));
+
+    //----------------- set push buttons stylesheet -------------------
+
+    ui->back_push->setStyleSheet(
+    "*{background-color:#ced5f5;"
+    "border:2px solid #0530f2;"
+    "border-radius:15px;}"
+    "*:hover{background: '#aeb6f5';}");
+
+    ui->login_push->setStyleSheet(
+    "*{background-color:#ced5f5;"
+    "border:2px solid #0530f2;"
+    "border-radius:35px;}"
+    "*:hover{background: '#aeb6f5';}");
+
+    //----------------- set line edits stylesheet -------------------------
+
+    ui->username_le->setStyleSheet("background-color:#e6e8fc; border:2px solid #828ff5; border-radius:10px;");
+    ui->password_le->setStyleSheet("background-color:#e6e8fc; border:2px solid #828ff5; border-radius:10px;");
+    ui->comboBox->setStyleSheet("background-color:#e6e8fc; border:2px solid #828ff5; border-radius:10px;");
 }
 
 Login_page::~Login_page()
